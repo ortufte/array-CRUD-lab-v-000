11bf538e2e2300d4adc8bdea2cc2baadb4f6e928
Olivia <ortufte@gmail.com>
2019-09-13 17:43:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["orange", "lime", "lemon", "grapefruit"] << "apple"
 end
 
 def add_element_to_start_of_array(array, element)
