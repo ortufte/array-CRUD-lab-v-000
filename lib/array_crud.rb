0371e798188b98ad19155d282ae9d2973ae59863
Olivia <ortufte@gmail.com>
2019-09-13 17:41:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [orange, lime, lemon, grapefruit]
+  citrus = [orange, lime, lemon, grapefruit]
 end
 
 def add_element_to_end_of_array(array, element)
